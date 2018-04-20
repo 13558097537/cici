4dfe0e7ada3d3c727def108f6e331e5c79d8aca9
1853586536@qq.com <975345162@qq.com>
2018-04-20 17:53:37 +0800

    1

---
+++ vue项目练习/src/app.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <p class="jj">点击我，会发生点事情</p>
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                
+            };
+        },
+        mounted() {
+
+        },
+        beforeDestroy() {
+
+        },
+        methods: {
+         $:( function () {
+          $(".jj").click(function(){
+            $(this).hide();
+        });       
+        })
+        }
+    };
+</script>
